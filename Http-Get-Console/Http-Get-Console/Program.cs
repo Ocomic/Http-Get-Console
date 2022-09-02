94f05546b2d853df484b4193489c4dd4de5299c1
Ocomic <108867917+Ocomic@users.noreply.github.com>
2022-09-02 12:52:33 +0200

    Version1
    - http get function

---
+++ Http-Get-Console/Http-Get-Console/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ConsoleApp1
+{
+    class program
+    {
+        public static string userInput = "dbXRates";
+        public static async Task Main(string[] args)
+        {
+
+            dynamic response = await program.makeRequest("https://data-asg.goldprice.org/"+userInput+"/USD");
+            Console.WriteLine(response);
+
+        }
+
+        public static async Task<dynamic> makeRequest(string url)
+        {
+            using var client = new HttpClient();
+
+            var result = await client.GetStringAsync(url);
+            dynamic json = JsonConvert.DeserializeObject<dynamic>(result);
+            return json;
+
+        }
+
+    }
+
+}
