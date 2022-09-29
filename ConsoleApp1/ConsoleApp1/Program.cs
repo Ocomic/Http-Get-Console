19d17c5b6113c32170099b18a763bb9238ca70f2
Ocomic <108867917+Ocomic@users.noreply.github.com>
2022-09-29 17:00:25 +0200

    json mock testdata
    - mocking json response from git url

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -20,7 +20,7 @@ namespace ConsoleApp1
         public static async Task Main(string[] args)
         {
 
-            dynamic response = await program.makeRequest("https://github.com/Ocomic/Http-Get-Console/blob/main/Testdata");
+            dynamic response = await program.makeRequest("https://raw.githubusercontent.com/Ocomic/Http-Get-Console/main/Testdata/70563885.json");
             Console.WriteLine(response); 
             //change (response) to (response.objectname) to enter a json object from
             //json "objectname" has to be changed to the name of the object in the file
