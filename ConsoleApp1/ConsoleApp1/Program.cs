95d4dd78443c8a0c1098802bf2c39dabc1af7a8e
Ocomic <108867917+Ocomic@users.noreply.github.com>
2022-10-04 15:33:30 +0200

    db.json change

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -21,7 +21,7 @@ namespace ConsoleApp1
         public static async Task Main(string[] args)
         {
 
-            dynamic response = await program.makeRequest("https://raw.githubusercontent.com/Ocomic/Http-Get-Console/main/Testdata/70563885.json");
+            dynamic response = await program.makeRequest("https://my-json-server.typicode.com/typicode/demo");
             Console.WriteLine(response); 
             //change (response) to (response.objectname) to enter a json object from
             //json "objectname" has to be changed to the name of the object in the file
